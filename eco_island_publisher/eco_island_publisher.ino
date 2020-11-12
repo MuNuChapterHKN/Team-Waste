b68df1bda5828096208a2297d666ec064ee4a01f
Xileny <xileny.seijasp@gmail.com>
2020-11-12 23:23:41 +0100

    client with temperature sensor

--- eco_island_publisher/eco_island_publisher.ino
+++ eco_island_publisher/eco_island_publisher.ino
@@ -1,8 +1,122 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Replace the next variables with your SSID/Password combination
+char ssid[23] = "REPLACE_WITH_YOUR_SSID"; // change number accordingly
+char password[27] = "REPLACE_WITH_YOUR_PASSWORD"; // change number accordingly
+
+// Add your MQTT Broker IP address
+const char* mqtt_server = "127.0.0.1";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+
+  // TODO check sensor
+  
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    
+    // Temperature in Celsius
+    int temperature = analogRead(A0);
+    float volt = (temperature/1024.0)*5.0;
+    float temp = (volt-0.5)*100;
+    Serial.print(temp);
+    Serial.println("degree C");
+    
+    // Convert the value to a char array
+    char tempString[8];
+    dtostrf(temp, 1, 2, tempString);
+    Serial.println("Temperature: ");
+    Serial.println(tempString);
+    client.publish("esp32/temperature", tempString);
+  }
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      //digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      //digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
