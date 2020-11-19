64769a23024b0db16f08014e8dc98ca99c927193
Riccardo Cuccu <57985742+RiccardoCuccu@users.noreply.github.com>
2020-11-19 17:41:12 +0100

    Add files

---
+++ eco_island_subscriber/ch_data.py
@@ -0,0 +1,21 @@
+import sqlite3
+from mysql.connector import Error
+
+
+def check_database(connection,filename):
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+    if c.fetchone()[0]==1 : 
+        print('Database exists.')
+    else :
+        print('Database does not exist.')
+        query = "CREATE DATABASE EPICS_project"
+        create_database(connection, query)
+        
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
