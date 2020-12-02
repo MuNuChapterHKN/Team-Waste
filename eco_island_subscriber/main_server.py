e2a2c8ea5066eb5c378c9d6b4237aa14c846ea0d
Gialbo <gilbertomanunza@gmail.com>
2020-12-03 00:31:50 +0100

    Bug fixing on database implementation

--- eco_island_subscriber/main_server.py
+++ eco_island_subscriber/main_server.py
@@ -10,11 +10,12 @@ def on_message(client, userdata, message):
     print("message retain flag=",message.retain)
     messaggio = str(message.payload.decode("utf-8"))
     #IDEAL FORMAT: bin_id SENSOR_ID timestamp samp --> separati da tab
-    fields = messaggio.split("\t")
+    fields = messaggio.split(" ")
+    print(fields)
     add_to_table = f"""
     INSERT INTO epics_table (bin_id,sensor_id,timestamp,samp) VALUES ({fields[0]},{fields[1]},{fields[2]},{fields[3]});
     """
-    connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
+    connection = create_db_connection("localhost", "root", "Rumapark97", "Epics_project") # Connect to the Database
     execute_query(connection, add_to_table) # Execute our defined query
 
 def create_database(connection, query):
@@ -32,6 +33,21 @@ def execute_query(connection, query):
         print("Query successful")
     except Error as err:
         print(f"Error: '{err}'")     
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
 def create_db_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
@@ -47,21 +63,22 @@ def create_db_connection(host_name, user_name, user_password, db_name):
 
     return connection
 
-
+connection = create_server_connection("localhost", "root", "Rumapark97") # Connect to the serve
 query = "CREATE DATABASE IF NOT EXISTS Epics_project"
 create_database(connection, query)
+connection = create_db_connection("localhost", "root", "Rumapark97", "Epics_project") # Directly connect to the database
 
 
 create_epics_table = """
-      CREATE TABLE IF NOT EXIST epics_table (
-      bin_id  int PRIMARY KEY,
-      sensor_id  int,
-      timestamp VARCHAR(40) NOT NULL,
-      samp VARCHAR(40) NOT NULL
+      CREATE TABLE IF NOT EXISTS epics_table (
+      bin_id  int NOT NULL PRIMARY KEY,
+      sensor_id  int NOT NULL PRIMARY KEY,
+      timestamp VARCHAR(40) NOT NULL PRIMARY KEY,
+      samp VARCHAR(40)
       );
-        """
+    """
         #cerca mysqu timestamp formato
-connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
+
 execute_query(connection, create_epics_table) # Execute our defined query
 
 
@@ -73,7 +90,7 @@ client = mqtt.Client() #create new instance
 print("connecting to broker")
 client.connect(broker_address,port=1883,keepalive=60) #connect to broker
 # keepalive: se il broker non risponde dopo deltaT, chiudi
-client.subscribe("sp32/sensor") # connessione al topic su cui pubblica il client
+client.subscribe("esp32/sensor") # connessione al topic su cui pubblica il client
 # qua va inserirto il topic a cui iscrivere il SUBSCRIBER
 
 client.on_message=on_message #leggo il messaggio arrivato dal broker
---
+++ eco_island_subscriber/mqtt_esp32_simulator.py
@@ -0,0 +1,9 @@
+
+import paho.mqtt.client as mqtt
+
+# This is the Publisher
+
+client = mqtt.Client()
+client.connect("localhost",1883,60)
+client.publish("esp32/sensor", "01 01 120320201220 23")
+client.disconnect()
