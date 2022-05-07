a398c61f6cbf05f830a6dd5155f9c8fdc8e076a1
Vincenzo Pellegrini <vincenzo.pellegrini.2001@gmail.com>
2022-05-07 21:23:09 +0200

    Done

---
+++ analytics_dashboard/frontend/src/lib/components/GranularitySelector.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte/internal";
+
+  import type { Granularity } from "./ChronChart.svelte";
+  export let options: Granularity[];
+
+  let selected = options[0] ?? null;
+
+  const dispatch = createEventDispatcher();
+  const onClick = (option) => {
+    dispatch("change", option);
+    selected = option;
+  }
+</script>
+
+<div class="tabs">
+{#each options as option}
+  <div class="tab tab-bordered" class:tab-active={option === selected} on:click={() => onClick(option)} >{option.toString()}</div>
+{/each}
+</div>
